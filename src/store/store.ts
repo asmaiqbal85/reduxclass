d22581e36959523bbdcaeaa47627a4fc49233742
asmaiqbal85 <aasmaiqbal85@gmail.com>
2024-09-27 03:14:17 -0700

    updated

---
+++ src/store/store.ts
@@ -0,0 +1,9 @@
+// src/store/store.ts
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
